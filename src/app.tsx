e2e0a10ac393d96b3a93ac4a7027a78315c3ad04
Masashi Hasegawa <mhasegawa.net@gmail.com>
2024-03-09 19:52:40 +0900

    css

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import ToneApp from './tone/ToneApp.tsx'
 import ScaleApp from './scale/scale_app.tsx'
 
 const App = () => {
+  const theme = createTheme({palette: {mode: 'light'}});
   return (
     <>
-      <ToneApp />
-      <ScaleApp />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <ToneApp />
+        <ScaleApp />
+      </ThemeProvider>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.tsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
