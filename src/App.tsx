274a2c1f4c23dcabe4e92823e43f0590b4cb66b5
Masashi Hasegawa <mhasegawa.net@gmail.com>
2024-03-24 18:38:59 +0900

    tab to change apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import React, {useState} from 'react';
 import ScaleApp from './scale/ScaleApp'
+import {Tabs, Tab} from '@mui/material';
+
+type AppType = "Overtone" | "Scale";
 
 const App = () => {
+  const [showingApp, setShowingApp] = useState<AppType>("Scale");
+  const handleChangeApp = (event: React.SyntheticEvent, newValue: string) => {
+    console.log("Event:", event)
+    console.log("newValue:", newValue)
+    setShowingApp(newValue as AppType);
+  };
+
+  const TabContent = () => {
+    console.log(showingApp)
+    switch (showingApp) {
+      case "Overtone":
+        return <Overtone />
+      case ("Scale"):
+        return <ScaleApp />
+      default:
+        return <></>
+    }
+  }
+
   return (
     <>
-        <ScaleApp />
+      <Tabs value={showingApp} onChange={handleChangeApp}>
+        <Tab value={"Scale"} label={"Scale"} />
+      </Tabs>
+
+      <TabContent />
     </>
   )
 }
